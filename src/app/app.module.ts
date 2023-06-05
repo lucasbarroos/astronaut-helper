4638f4095f6ff5bdb81a3831b0a8a67f9728c12f
lucasbarroos <lbarros.comp@gmail.com>
2023-06-05 00:35:03 -0300

    feat: adding the blank animation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AnimationComponent } from './home/components/animation/animation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AnimationComponent,
   ],
   imports: [
     BrowserModule,
