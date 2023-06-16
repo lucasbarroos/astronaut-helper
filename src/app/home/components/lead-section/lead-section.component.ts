4d0f4bab052021964f0d6e897ebefc6c8225ec2f
lucasbarroos <lbarros.comp@gmail.com>
2023-06-16 16:27:05 -0300

    feat: adding the lead section

--- src/app/home/components/lead-section/lead-section.component.ts
+++ src/app/home/components/lead-section/lead-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lead-section',
@@ -8,18 +9,40 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class LeadSectionComponent implements OnInit {
 
   @Output()
-  handleSubmitClicked: EventEmitter<any> = new EventEmitter();
+  onSubmit: EventEmitter<any> = new EventEmitter();
+
+  requireFieldsValidors = [
+    Validators.required,
+    Validators.minLength(2),
+  ];
+
+  showErrors = false;
+
+  public formGroup = new FormGroup({
+    name: new FormControl('', this.requireFieldsValidors),
+    email: new FormControl('', this.requireFieldsValidors),
+  });
 
   constructor() {
 
   }
 
   ngOnInit() {
-  
+  }
+
+  isValidField(name: string) {
+    const form = this.formGroup.get(name);
+    return form?.value && form?.valid;
   }
 
   handleSubmit() {
-    this.handleSubmitClicked.emit({});
+    this.showErrors = true;
+    if (this.isValidField('name') && this.isValidField('email')) {
+      this.onSubmit.emit({
+        name: this.formGroup.get('name')?.value,
+        email: this.formGroup.get('email')?.value,
+      });
+    }
   }
 
 }
