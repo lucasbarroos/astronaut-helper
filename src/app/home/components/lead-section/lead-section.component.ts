76992a9f0bd4afe35bd63216c567ad8b7bc10ae7
lucasbarroos <lbarros.comp@gmail.com>
2023-06-16 15:30:57 -0300

    feat: adding the lead section

--- src/app/home/components/lead-section/lead-section.component.ts
+++ src/app/home/components/lead-section/lead-section.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-lead-section',
   templateUrl: './lead-section.component.html',
   styleUrls: ['./lead-section.component.scss']
 })
-export class LeadSectionComponent {
+export class LeadSectionComponent implements OnInit {
+
+  @Output()
+  handleSubmitClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  
+  }
+
+  handleSubmit() {
+    this.handleSubmitClicked.emit({});
+  }
 
 }
