ce227e4af814d28223523db5bcf45d64093f1a28
lucasbarroos <lbarros.comp@gmail.com>
2023-06-16 13:57:21 -0300

    lint: linting the files

--- src/app/home/components/action-button/action-button.component.ts
+++ src/app/home/components/action-button/action-button.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ActionButtonComponent {
 
   @Input()
-  title: string = 'Help Me';
+  title = 'Help Me';
   
   @Output()
   handleClick: EventEmitter<void> = new EventEmitter();
