386e292406e5f10163e4e9fac5f717e2111f2e89
lucasbarroos <lbarros.comp@gmail.com>
2023-06-16 14:13:57 -0300

    feat: register the about section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AnimationComponent } from './home/components/animation/animation.component';
 import { ActionButtonComponent } from './home/components/action-button/action-button.component';
+import { AboutSectionComponent } from './home/components/about-section/about-section.component';
 
 export function playerFactory() {
   return player;
@@ -19,6 +20,7 @@ export function playerFactory() {
     HomeComponent,
     AnimationComponent,
     ActionButtonComponent,
+    AboutSectionComponent,
   ],
   imports: [
     BrowserModule,
