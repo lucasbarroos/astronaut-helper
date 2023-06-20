ad56e1a0a02214a4db93606906c4b10d0a998d9a
Lucas Barros <lbarros.comp@gmail.com>
2023-06-20 22:45:57 +0000

    feat: adding the new service import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommentsService } from './services/comments.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(commentsService: CommentsService) { }
+
+  ngOnInit() {
+
+  }
+
   homepageActionButtonClicked() {
     const el = document.getElementById('leadSection');
     el?.scrollIntoView({ behavior: 'smooth' });
